969e4ed7a45f0039ec67fd1b8877d237d719660c
SallyResch <sallyresch27@gmail.com>
2026-02-13 13:03:44 +0100

    added router-page connection for index

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ const __dirname = path.resolve();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.render(
+        path.join(__dirname, "/views/pages/index"),
+    )
+})
+
 app.listen(port, () => console.log(`Listening to port ${port}`))
