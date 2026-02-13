24fe80c8afd33ed88d8a35cb85bb321c7d423f1d
SallyResch <sallyresch27@gmail.com>
2026-02-13 13:04:55 +0100

    added imports for routers in routers folder

---
+++ routers/autumnRouter.js
@@ -0,0 +1,3 @@
+import express from "express"
+import * as path from "path"
+import { autumn } from '../data/data.js'
---
+++ routers/winterRouter.js
@@ -0,0 +1,3 @@
+import express from "express"
+import * as path from "path"
+import { winter } from '../data/data.js'
